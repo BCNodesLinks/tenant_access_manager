c7cf57a392f54f9fa1b8c4ba64c90398fe511b10
Ben Campbell <ben.campbell@nodeslinks.com>
2024-10-07 12:43:44 +0100

    updated

--- tenant-access-manager.php
+++ tenant-access-manager.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Tenant Access Manager
 Description: Allows admin to create tenants with access to specific resources and restricts site access until email confirmation.
-Version: 1.8
+Version: 1.9
 Author: Ben Campbell
 */
 
@@ -14,29 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'TAM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'TAM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-// Define a secret key for token generation (replace with your secure key)
-if ( ! defined( 'TAM_SECRET_KEY' ) ) {
-    define( 'TAM_SECRET_KEY', 'O*cx3sWWeaX|:Eg=qFm8$ky:IhM=T%#p;iybWlzOK,6qwoy{`>5v&!4hszV~ww|0' );
-}
-
-// Enable WordPress debug logging (ensure this is appropriate for your environment)
-if ( ! defined( 'WP_DEBUG' ) ) {
-    define( 'WP_DEBUG', true );
-}
-if ( ! defined( 'WP_DEBUG_LOG' ) ) {
-    define( 'WP_DEBUG_LOG', true );
-}
-if ( ! defined( 'WP_DEBUG_DISPLAY' ) ) {
-    define( 'WP_DEBUG_DISPLAY', false );
-}
-
-// Define Customer.io credentials (replace with your actual credentials)
-if ( ! defined( 'CUSTOMERIO_SITE_ID' ) ) {
-    define( 'CUSTOMERIO_SITE_ID', '33452c4a9f7be138382c' );
-}
-if ( ! defined( 'CUSTOMERIO_API_KEY' ) ) {
-    define( 'CUSTOMERIO_API_KEY', 'a6f9268fa3464ebe5f2c' );
-}
 
 // Define a prefix for Customer.io events
 if ( ! defined( 'TAM_EVENT_PREFIX' ) ) {
